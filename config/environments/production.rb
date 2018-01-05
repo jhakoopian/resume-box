b02fa3ea4ca30149a2e32a8da69e0dbcf71b725e
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2018-01-05 13:37:50 -0500

    heroku mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     }
   }
 
-  config.action_mailer.default_url_options = { host: 'sheltered-forest-43403.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'radiant-journey-75150.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
