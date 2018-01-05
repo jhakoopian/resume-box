8314c4e2cee97e406e4294f2671ca5c9703c43b5
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2018-01-05 13:58:22 -0500

    production mailer config fix two

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     }
   }
 
-  config.action_mailer.default_url_options = { host: 'radiant-journey-75150.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://radiant-journey-75150.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
