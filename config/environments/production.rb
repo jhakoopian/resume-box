5a156ea0c88e2926c24d3eacd62d6dbea3ea4740
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2018-01-05 13:24:37 -0500

    production config fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'sheltered-forest-43403.herokuapp.com' }
-
   config.paperclip_defaults = {
     storage: :s3,
     s3_region: ENV.fetch('AWS_REGION'),
@@ -88,5 +86,18 @@ Rails.application.configure do
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     }
+  }
+
+  config.action_mailer.default_url_options = { host: 'sheltered-forest-43403.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
   }
 end
